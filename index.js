7d91d8d4006e37ba0ec48c6b083a840a60f41cc7
Daenerysissohot <jeremie.vancutsem@gmail.com>
2018-05-17 12:22:42 +0200

    gamereviewversionalpha

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 // const {database, Post, Vote } = require('./models');
 const Sequelize = require('sequelize');
 const app = express();
+
 app.use(express.static("public"));
 
 app.set('view engine','pug');
@@ -21,21 +23,31 @@ app.get('/', (req, res ) => {
         .then(posts => res.render('home', {posts}));
 });
 
+/*
 app.post('/api/post', (req,res) => {
     const { title,content } = req.body;
     Post
         .create({title,content})
         .then(()=> res.redirect('/'))
 });
+*/
 app.post('/api/post', (req,res) => {
     const  title = req.body.title;
+    const review = req.body.review;
+    const eval = req.body.eval;
     Post
-        .create({title,title})
+        .create({title,review,eval})
         .then(()=> {
             res.redirect('/')
         })
 });
 
+
+
+
+
+
+/*
 const User = db.define ('user', {
     fullname: { type: Sequelize.STRING },
     email: { type: Sequelize.STRING }
@@ -47,14 +59,14 @@ User
     .sync()
     .then(() => {
         User.create({
-            fullname: 'Jane Doe',
-            email: 'jane.doe@gmail.com'
+            fullname: 'Lara Croft',
+            email: 'Lara.croft@gmail.com'
         });
     })
     .then(() => {
         User.create({
-            fullname: 'John Doe',
-            email: 'john.doe@gmail.com'
+            fullname: 'Solid snake',
+            email: 'solid.snake@gmail.com'
         });
     })
     .then(() => {
@@ -63,7 +75,7 @@ User
     .then((users) => {
         console.log(users);
     });
-
+*/
 app.post('/api/post/:postID/upvote', (req, res) => {
     Vote
         .create({ action: 'up',postId: req.params.postId})
@@ -77,8 +89,15 @@ app.post('/api/post/:postID/downvote', (req, res) => {
 });
 
 
+
 const Post = db.define('post', {
     title:{
+        type:Sequelize.STRING
+    },
+    eval:{
+        type:Sequelize.STRING
+    },
+    review:{
         type:Sequelize.STRING
     }
 });
@@ -87,7 +106,28 @@ Post
     .sync()
     .then(()=>{
         Post.create({
-            title: "1er post"
+            title: "Fortnite",
+            eval:"4",
+            review:" super jeux !"
+        })
+    });
+Post
+.sync()
+    .then(()=>{
+        Post.create({
+            title: "PUBG",
+            eval:"2",
+            review: " c'est nul "
+
+        })
+    });
+Post
+.sync()
+    .then(()=>{
+        Post.create({
+            title: "OverWatch",
+            eval:"3",
+            review: " ça va "
         })
     });
 
@@ -96,10 +136,6 @@ const Vote = db.define('vote', {
         type: Sequelize.ENUM('up','down')
     }
 });
-
-
-
-
 
 Post.hasMany(Vote);
 
